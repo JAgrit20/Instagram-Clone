1bbc4c8cec79632c47e54f8939ac2c54920b0182
JArit20 <jagritacharya2020@gmail.com>
2020-11-11 20:45:54 +0530

    signIn route created

--- server/keys.js
+++ server/keys.js
@@ -1,3 +1,4 @@
 module.exports={
     MONGOURL:"mongodb+srv://admin:JVuMIO4n7ifnrRu0@cluster0.u7pkn.mongodb.net/users?retryWrites=true&w=majority"
+    ,JWT_SECRET:"jagritmadethis"
 }
--- server/routes/auth.js
+++ server/routes/auth.js
@@ -3,6 +3,8 @@ const router = express.Router();
 const mongoose = require('mongoose');
 const User = mongoose.model('User');
 const bcrypt = require('bcryptjs');
+const jwt = require('jsonwebtoken');
+const {JWT_SECRET} = require('../keys')
 
 
 // router.get('/',(req, res) => {
@@ -53,6 +55,9 @@ router.post('/signin',(req, res) => {
         bcrypt.compare(password,savedUser.password).then(doMatch=>{
             if(doMatch){
                 // res.json({message:"successfully matched"})
+              
+                const token = jwt.sign({_id:savedUser.id},JWT_SECRET)
+                res.json({token:token})
             }
             else{
                 return res.status(422).json({error:"invalid credentials"})
